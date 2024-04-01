90eb621728e704398f9124dd003369c7c6f53726
vaijenyavaiju1 <vaijenyavaiju123@gamil.com>
2024-04-01 04:24:06 +0000

    solution

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,38 @@
+package com.example.todo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import com.example.todo.service.TodoJpaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.todo.model.Todo;
+
+@RestController
+public class TodoController {
+    @Autowired
+    public TodoJpaService todoService;
+
+    @GetMapping("/todos")
+    public ArrayList<Todo> getTodos() {
+        return todoService.getTodos();
+    }
+
+    @GetMapping("/todos/{id}")
+    public Todo getTodoById(@PathVariable("id") int id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PostMapping("/todos")
+    public Todo addTodo(@RequestBody Todo todo) {
+        return todoService.addTodo(todo);
+    }
+
+    @PutMapping("/todos/{id}")
+    public Todo updateTodo(@PathVariable("id") int id, @RequestBody Todo todo) {
+        return todoService.updateTodo(id, todo);
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public void deleteTodo(@PathVariable("id") int id) {
+        todoService.deleteTodo(id);
+    }
+}
---
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,16 @@
+package com.example.todo.repository;
+
+import com.example.todo.model.Todo;
+import java.util.*;
+
+public interface TodoRepository {
+    ArrayList<Todo> getTodos();
+
+    Todo getTodoById(int id);
+
+    Todo addTodo(Todo todo);
+
+    Todo updateTodo(int id, Todo todo);
+
+    void deleteTodo(int id);
+}
